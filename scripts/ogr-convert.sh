ee2fbb626214fdf98fd25544c1600a83465a3ac7
Simon Templer <simon@wetransform.to>
2016-06-05 11:22:17 +0200

    added ogr2ogr execution

--- scripts/ogr-convert.sh
+++ scripts/ogr-convert.sh
@@ -26,9 +26,10 @@ then
   source_file=${cmdarg_cfg['source-name']}
   if [ -z "$source_file" ]; then echo "ERROR: Source file name must be set if file is downloaded from a URL"; exit 1; fi
   mkdir "${DATA_DIR}/source" || true
-
+  echo "Downloading file..."
   curl $source_loc > "${DATA_DIR}/source/${source_file}"
   rc=$?; if [ $rc -ne 0 ]; then echo "ERROR: Error downloading source file"; exit $rc; fi
+  # use downloaded file as new source
   source_loc="${DATA_DIR}/source/${source_file}"
 fi
 
@@ -38,5 +39,45 @@ if [ ! -f $source_loc ]; then
     exit 1;
 fi
 
+# check file mime type
 source_mime=$(file -b --mime-type $source_loc)
 echo "Mime-type for source file: $source_mime"
+
+# extract if ZIP file
+if [ "$source_mime" == "application/zip" ]; then
+  echo "Source file is a ZIP file - extracting..."
+  mkdir "${DATA_DIR}/source-zip" || true
+  unzip "$source_loc" -d "${DATA_DIR}/source-zip"
+  # use extracted content as new source
+  source_loc="${DATA_DIR}/source-zip"
+fi
+
+# build ogr2ogr comman
+convert_cmd="ogr2ogr"
+
+source_srs=${cmdarg_cfg['source-srs']}
+target_srs=${cmdarg_cfg['target-srs']}
+
+if [ -n "$source_srs" ]; then
+  # source srs is provided
+  # add as parameter
+  convert_cmd="$convert_cmd -s_srs \"$source_srs\""
+  if [ -z "$target_srs" ]; then
+    # no target srs, but if s_srs is provided, t_srs must be as well
+    target_srs=$source_srs
+  fi
+fi
+if [ -n "$target_srs" ]; then
+  # target srs is provided
+  # add as parameter
+  convert_cmd="$convert_cmd -t_srs \"$target_srs\""
+fi
+
+target_loc=${cmdarg_cfg['out']}
+target_format=${cmdarg_cfg['target-format']}
+convert_cmd="$convert_cmd -f \"$target_format\" \"$target_loc\" \"$source_loc\""
+
+# run
+echo "Executing conversion..."
+eval $convert_cmd
+rc=$?; if [ $rc -ne 0 ]; then echo "ERROR: Conversion failed"; exit $rc; else echo "Conversion successful"; fi
